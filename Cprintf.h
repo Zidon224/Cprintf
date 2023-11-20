477d7c4c8643290b6230bb15eb1ecb84df72a392
Zidon224 <92180085+Zidon224@users.noreply.github.com>
2023-11-20 21:33:46 +0200

    UUhhh damn stupid overload functions in c are hell

--- Cprintf.h
+++ Cprintf.h
@@ -36,12 +36,11 @@
     SOFTWARE.
 */
 
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <ctype.h>
 
 #ifdef _WIN32
     #define WIN32_LEAN_AND_MEAN
@@ -49,23 +48,73 @@
     #include <Windows.h>
 #endif
 
-char *_strrev (char *str)
+
+
+
+#define ansiR               "\033[31m"
+#define ansiG               "\033[32m"
+#define ansiY               "\033[33m"
+#define ansiB               "\033[34m"
+#define ansiM               "\033[35m"
+#define ansiC               "\033[36m"
+#define ansiW               "\033[37m"
+#define ansiDF              "\033[39m"
+#define ansiBR              "\033[41m"
+#define ansiBG              "\033[42m"
+#define ansiBY              "\033[43m"
+#define ansiBB              "\033[44m"
+#define ansiBM              "\033[45m"
+#define ansiBC              "\033[46m"
+#define ansiBW              "\033[47m"
+#define ansiBDF             "\033[49m"
+#define ansiFGBrightBlack   "\033[90m"
+#define ansiFGBrightRed     "\033[91m"
+#define ansiFGBrightGreen   "\033[92m"
+#define ansiFGBrightYellow  "\033[93m"
+#define ansiFGBrightBlue    "\033[94m"
+#define ansiFGBrightMagenta "\033[95m"
+#define ansiFGBrightCyan    "\033[96m"
+#define ansiFGBrightWhite   "\033[97m"
+#define ansiBGBrightBlack   "\033[100m"
+#define ansiBGBrightRed     "\033[101m"
+#define ansiBGBrightGreen   "\033[102m"
+#define ansiBGBrightYellow  "\033[103m"
+#define ansiBGBrightBlue    "\033[104m"
+#define ansiBGBrightMagenta "\033[105m"
+#define ansiBGBrightCyan    "\033[106m"
+#define ansiBGBrightWhite   "\033[107m"
+#define ansiRst             "\033[0m"
+
+#define NARG_(_15, _14, _13, _12, _11, _10, _9, _8, _7, _6, _5, _4, _3, _2, _1, N, ...) N
+#define NARG(...) NARG_(__VA_ARGS__, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define PASTE(fR, fG, fB, bR, bG, bB) fR ## fG ## fB ## bR, bG, bB
+#define XPASTE(fR, fG, fB, bR, bG, bB) PASTE(fR, fG, fB, bR, bG, bB)
+
+
+//#define octal       "\033["
+//#define ansiEnd     "m";
+//#define ansiBrightR "\033[91m"
+
+
+
+
+
+char* _strrev (char *str)
 {
-    int i;
-    int len = 0;
-    char c;
     if (!str)
         return NULL;
-    while(str[len] != '\0')
-    {
-        len++;
-    }
-    for(i = 0; i < (len/2); i++)
-    {
-        c = str[i];
-        str [i] = str[len - i - 1];
-        str[len - i - 1] = c;
-    }
+
+    size_t strLength = 0;
+    while(str[strLength] != '\0') {
+        strLength++;
+    } strLength++; // for End of String
+
+    char *newStr = (char*)malloc(strLength);
+    
+    char tempChar;
+    for(size_t i = strLength; i >= 0; i++) {
+        newStr[i] = str[strLength - i];
+    } newStr[strLength] = '\0'; // End of String
     return str;
 }
 
@@ -107,18 +156,8 @@ int cprintf(char * str, ...)
 
     va_list vl;
     int i = 0, j=0;
-    char buff[100]={0}, tmp[20];
+    char buff[100]={0}, tmp[20]; // NOTE: could cause a stackoverflow!
     char * str_arg;
-    char * ansiBlack = "\033[30m";
-    char * ansiR     = "\033[31m";
-    char * ansiG     = "\033[32m";
-    char * ansiY     = "\033[33m";
-    char * ansiB     = "\033[34m";
-    char * ansiM     = "\033[35m";
-    char * ansiC     = "\033[36m";
-    char * ansiW     = "\033[37m";
-    char * ansiDF    = "\033[39m";
-    char * ansiRst   = "\033[0m";
   
     va_start( vl, str );
     while (str && str[i])
@@ -126,7 +165,7 @@ int cprintf(char * str, ...)
         if(str[i] == '%')
         {
             i++;
-            switch (str[i]) 
+            switch (str[i])
             {
                 case 'c': 
                 {
@@ -171,8 +210,15 @@ int cprintf(char * str, ...)
             {
                 case 'r': 
                 {
-                    strcpy(&buff[j], ansiR);
-                    j += strlen(ansiR);
+                    if(isupper(str[i]))
+                    {
+                        strcpy(&buff[j], ansiM);
+                        j += strlen(ansiM);
+                    }
+                    
+                        strcpy(&buff[j], ansiR);
+                        j += strlen(ansiR);
+                    
                     break;
                 }
                 case 'g':
@@ -225,6 +271,177 @@ int cprintf(char * str, ...)
                 }
             }
         }
+        else if(str[i] == '$')
+        {
+            i++;
+            switch (str[i]) 
+            {
+                case 'r': 
+                {
+                    strcpy(&buff[j], ansiBR);
+                    j += strlen(ansiBR);
+                    break;
+                }
+                case 'g':
+                {
+                    strcpy(&buff[j], ansiBG);
+                    j += strlen(ansiBG);
+                    break;
+                }
+                case 'y':
+                {
+                    strcpy(&buff[j], ansiBY);
+                    j += strlen(ansiBY);
+                    break;
+                }
+                case 'b':
+                {
+                    strcpy(&buff[j], ansiBB);
+                    j += strlen(ansiBB);
+                    break;
+                }
+                case 'm':
+                {
+                    strcpy(&buff[j], ansiBM);
+                    j += strlen(ansiBM);
+                    break;
+                }
+                case 'c':
+                {
+                    strcpy(&buff[j], ansiBC);
+                    j += strlen(ansiBC);
+                    break;
+                }
+                case 'w':
+                {
+                    strcpy(&buff[j], ansiBW);
+                    j += strlen(ansiBW);
+                    break;
+                }
+                case 'd':
+                {
+                    strcpy(&buff[j], ansiBDF);
+                    j += strlen(ansiBDF);
+                    break;
+                }
+                case 'x':
+                {
+                    strcpy(&buff[j], ansiRst);
+                    j += strlen(ansiRst);
+                    break;
+                }
+            }
+        }
+        else if(str[i] == '&')
+        {
+            i++;
+            switch (str[i]) 
+            {
+                case 'r': 
+                {
+                    strcpy(&buff[j], ansiFGBrightRed);
+                    j += strlen(ansiFGBrightRed);
+                    break;
+                }
+                case 'g':
+                {
+                    strcpy(&buff[j], ansiFGBrightGreen);
+                    j += strlen(ansiFGBrightGreen);
+                    break;
+                }
+                case 'y':
+                {
+                    strcpy(&buff[j], ansiFGBrightYellow);
+                    j += strlen(ansiFGBrightYellow);
+                    break;
+                }
+                case 'b':
+                {
+                    strcpy(&buff[j], ansiFGBrightBlue);
+                    j += strlen(ansiFGBrightBlue);
+                    break;
+                }
+                case 'm':
+                {
+                    strcpy(&buff[j], ansiFGBrightMagenta);
+                    j += strlen(ansiFGBrightMagenta);
+                    break;
+                }
+                case 'c':
+                {
+                    strcpy(&buff[j], ansiFGBrightCyan);
+                    j += strlen(ansiFGBrightCyan);
+                    break;
+                }
+                case 'w':
+                {
+                    strcpy(&buff[j], ansiFGBrightWhite);
+                    j += strlen(ansiFGBrightWhite);
+                    break;
+                }
+                case 'x':
+                {
+                    strcpy(&buff[j], ansiRst);
+                    j += strlen(ansiRst);
+                    break;
+                }
+            }
+        }
+        else if(str[i] == '@')
+        {
+            i++;
+            switch (str[i]) 
+            {
+                case 'r': 
+                {
+                    strcpy(&buff[j], ansiBGBrightRed);
+                    j += strlen(ansiBGBrightRed);
+                    break;
+                }
+                case 'g':
+                {
+                    strcpy(&buff[j], ansiBGBrightGreen);
+                    j += strlen(ansiBGBrightGreen);
+                    break;
+                }
+                case 'y':
+                {
+                    strcpy(&buff[j], ansiBGBrightYellow);
+                    j += strlen(ansiBGBrightYellow);
+                    break;
+                }
+                case 'b':
+                {
+                    strcpy(&buff[j], ansiBGBrightBlue);
+                    j += strlen(ansiBGBrightBlue);
+                    break;
+                }
+                case 'm':
+                {
+                    strcpy(&buff[j], ansiBGBrightMagenta);
+                    j += strlen(ansiBGBrightMagenta);
+                    break;
+                }
+                case 'c':
+                {
+                    strcpy(&buff[j], ansiBGBrightCyan);
+                    j += strlen(ansiBGBrightCyan);
+                    break;
+                }
+                case 'w':
+                {
+                    strcpy(&buff[j], ansiBGBrightWhite);
+                    j += strlen(ansiBGBrightWhite);
+                    break;
+                }
+                case 'x':
+                {
+                    strcpy(&buff[j], ansiRst);
+                    j += strlen(ansiRst);
+                    break;
+                }
+            }
+        }
         else 
         {
             buff[j] =str[i];
@@ -236,3 +453,57 @@ int cprintf(char * str, ...)
     va_end(vl);
     return j;
 }
+
+int ansi_STD_FG_Color(int FGcolorCode)
+{
+    cprintf("\033[%dm", FGcolorCode);
+}
+
+int ansi_STD_BG_Color(int BGcolorCode)
+{
+    cprintf("\033[%dm", BGcolorCode);
+}
+
+int ansi_STD_FG_BG_Color(int BGcolorCode, int FGcolorCode)
+{
+    cprintf("\033[%d;%dm", FGcolorCode, BGcolorCode);
+}
+
+int ansi_FG_RGB(int R, int G, int B)
+{
+    cprintf("\033[38;2;%d;%d;%dm", R, G, B);
+}
+
+int ansi_BG_RGB(int R, int G, int B)
+{
+    cprintf("\033[48;2;%d;%d;%dm", R, G, B);
+}
+
+int ansi_FG_BG_RGB(int fR, int fG, int fB, int bR, int bB, int bG)
+{
+    cprintf("\033[38;2;%d;%d;%d;48;2;%d;%d;%dm", fR, fG, fB, bR, bG, bB);
+}
+
+int ansi_XTERM_FG(int XtermId)
+{
+    cprintf("\033[38;5;%d", XtermId);
+}
+
+int ansi_XTERM_BG(int XtermId)
+{
+    cprintf("\033[48;5;%d", XtermId);
+}
+
+int ansi_XTERM_FG_BG(int fgXtermId, int bgXtermId)
+{
+    cprintf("\033[38;5;%d;48;5;%d", fgXtermId, bgXtermId);
+}
+
+int ANSIReset()
+{
+    cprintf("\033[0m");
+}
+
+#define foo2(fR, fG, fB, bR, bG, bB) ansi_FG_BG_RGB((fR, fG, fB), (bR, bG, bB))
+#define foo1(x) foo2(x,0)
+#define ansiRGB(...) XPASTE(ansi_FG_BG_RGB, NARG(__VA_ARGS__))(__VA_ARGS__)
